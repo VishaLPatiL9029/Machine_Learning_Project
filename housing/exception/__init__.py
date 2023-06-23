d0175b303f90a1423f3d25f3e23ae81c9997ccb6
VishaLPatiL9029 <vishalpatil228@gmail.com>
2023-06-23 23:23:39 +0530

    updated code

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -6,10 +6,8 @@ class housingException(Exception):
     def __init__(self, error_message : Exception, error_detail:sys):
         super().__init__(error_message)
         Exception(error_message)
-        self.error_message = HousingException.get_detailed_error_message(error_message=error_message,
-                                                                       error_detail=error_detail
-                                                                        )
-
+        self.error_message = error_message
+        
     @staticmethod
     def get_detailed_error_message(error_message:Exception, error_detail:sys) -> str:
         """
